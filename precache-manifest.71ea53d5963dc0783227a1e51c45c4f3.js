3ea32c1c79c98638b3d60c5bb8e6972d868943fb
jarvis394 <jarvis394@users.noreply.github.com>
2020-11-03 18:14:20 +0000

    deployed via Github Pages Deploy Action 🚀 for 2e3145fd076c28d36c1dbb4a43b7130170f2786e

---
+++ precache-manifest.71ea53d5963dc0783227a1e51c45c4f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7729e8f7d6b34f7c864c7951adac89a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41eb5dd83727cca9d984",
+    "url": "/static/css/2.9905d55a.chunk.css"
+  },
+  {
+    "revision": "41eb5dd83727cca9d984",
+    "url": "/static/js/2.e92fa3a9.chunk.js"
+  },
+  {
+    "revision": "4262fbd88ef667a896128fa7f032257f",
+    "url": "/static/js/2.e92fa3a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ef5105756bd85cf976a",
+    "url": "/static/js/main.05459474.chunk.js"
+  },
+  {
+    "revision": "fe80b5c065de840298e8",
+    "url": "/static/js/runtime-main.c05885d4.js"
+  }
+]);
